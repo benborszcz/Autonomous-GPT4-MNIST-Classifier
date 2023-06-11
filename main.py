c4c8abc634072d89aea0c662da256e572d31fd0b
Ben Borszcz <ben.borszcz@gmail.com>
2023-06-11 17:59:37 -0400

    works with GPT-3.5

--- main.py
+++ main.py
@@ -31,10 +31,7 @@ def mnist_run(device, num_filters, kernel_size, hidden_sizes, lin_dropout, conv_
     # Combine train and evaluation history
     history = train_history + [eval_history]
 
-    # Get the model structure
-    model_structure = str(classifier.net)
-
-    return history, model_structure
+    return history
 
 import re
 
@@ -61,20 +58,35 @@ current_hyperparams = {
     "num_filters": [32,64],
     "kernel_size": 3,
     "hidden_sizes": [128,64],
-    "learning_rate": 0.001,
-    "epochs": 1,
+    "learning_rate": 0.01,
+    "epochs": 10,
     "batch_size": 100,
     "lin_dropout": 0.5,
     "conv_dropout": 0.25,
 }    
 
+
+def generate_readable_history(history):
+    ret = ""
+    for instance in history:
+        ret += f"Pass {instance[0]['pass']}:\n"
+        total_epochs = 0
+        for item in instance[1:-1]:
+            if 'epoch' in item:
+                total_epochs += 1
+            elif 'evaluation' in item:
+                ret += f"  Evaluation {item['evaluation']}: Accuracy = {item['accuracy']}\n"
+        ret += f"  Total Epochs: {total_epochs}\n"
+        ret += f"  Hyperparameters: {instance[-1]['hyperparameters']}\n\n"
+    return ret
+
 agent = TrainerAgent()
 history = []
 def handle_think_action(history, current_hyperparams, thought_message=None):
-    model_output = agent.generate_training_action(history, current_hyperparams, thought_message=thought_message)
-    print("\n****")
+    model_output = agent.generate_training_action(generate_readable_history(history), current_hyperparams, thought_message=thought_message)
+    print("\n**** Asking GPT for follow up ****")
     print(model_output)
-    print("****")
+
 
     parsed_output = parse_model_output(model_output)
 
@@ -83,7 +95,7 @@ def handle_think_action(history, current_hyperparams, thought_message=None):
         response_content = parsed_output["response_content"]
 
         if action == "Think":
-            return handle_think_action(history, current_hyperparams, thought_message=response_content)
+            return handle_think_action(history, current_hyperparams, thought_message=model_output)
         else:
             return action, response_content
     else:
@@ -91,13 +103,15 @@ def handle_think_action(history, current_hyperparams, thought_message=None):
         return None, None
 
 
-for i in range(10):
-    print(f"\n\n################PASS {i}################")
-    history.append(mnist_run(device, **current_hyperparams))
-    print("\n****")
-    model_output = agent.generate_training_action(history, current_hyperparams)
+for i in range(20):
+    print(f"\n\n#### PASS {i} ####")
+    instance = mnist_run(device, **current_hyperparams)
+    instance.insert(0, {"pass": f"{i}"})
+    instance.append({"hyperparameters": str(current_hyperparams)})
+    history.append(instance)
+    print("\n**** Asking GPT for Changes ****")
+    model_output = agent.generate_training_action(generate_readable_history(history), current_hyperparams)
     print(model_output)
-    print("****")
 
     parsed_output = parse_model_output(model_output)
 
@@ -107,6 +121,7 @@ for i in range(10):
 
         if action == "Change":
             new_hyperparams = eval(response_content.strip("hyperparams ="))
+            print("\nOld hyperparameters:", current_hyperparams)
             current_hyperparams.update(new_hyperparams)
             print("Updated hyperparameters:", current_hyperparams)
 
@@ -114,10 +129,11 @@ for i in range(10):
             print("Rerunning the model with the same hyperparameters.")
 
         elif action == "Think":
-            action, response_content = handle_think_action(history, current_hyperparams, thought_message=response_content)
-            print(action)
+            action, response_content = handle_think_action(history, current_hyperparams, thought_message=model_output)
+
             if action == "Change":
                 new_hyperparams = eval(response_content.strip("hyperparams ="))
+                print("\nOld hyperparameters:", current_hyperparams)
                 current_hyperparams.update(new_hyperparams)
                 print("Updated hyperparameters:", current_hyperparams)
 
--- trainer_agent.py
+++ trainer_agent.py
@@ -14,23 +14,20 @@ class TrainerAgent():
     def clear_messages(self):
         self.messages = []
 
-    def generate_training_action(self, history: List[dict], hyperparams: dict, thought_message = None, model: str = "gpt-4", temperature: float = 0.2, presence_penalty: float = 0.1, frequency_penalty: float = 0.1, max_tokens: int = 1000):
+    def generate_training_action(self, history, hyperparams: dict, thought_message = None, model: str = "gpt-3.5-turbo", temperature: float = 0.1, presence_penalty: float = 0.0, frequency_penalty: float = 0.0, max_tokens: int = 1000):
         self.add_message("You are an AI model specialized in neural network training, you respond only in the format requested and may only take the actions provided.", role="system")
-        self.add_message("Here is the history and hyperparams of the neural network training:", role="user")
-        self.add_message(str(history), role="user")
-        self.add_message(str(hyperparams), role="user")
 
         formatting = ("""
+        Here is the history of the neural network training, note that all of the changes in model hyperparams were made by you:
+        """+
+        str(history)
+        +"""
+
         You are provided these actions:
-        # "Rerun" - reruns the model with the same hyperparameters that is being trained on the MNIST data
-        ## Specified Response: hyperparams = {"num_filters": list len >= 1, "kernel_size": int>=1, "hidden_sizes": list len >= 1, "learning_rate": float<1.0, "epochs": int>=1, "batch_size": int,"lin_dropout": float<1.0, "conv_dropout": float<1.0}
-        
         # "Change" - changes the model hyperparameters that is being trained on the MNIST data
-        ## Specified Response: RUN
-        
+        ## Specified Response: hyperparams = {"num_filters": list with length>=1, "kernel_size": int>=1, "hidden_sizes": list with length>=1, "learning_rate": float<1.0, "epochs": int>=1, "batch_size": int,"lin_dropout": float<1.0, "conv_dropout": float<1.0}
         # "Think" - allows you to think and write about the changes to be made next. This can only be done ONCE
         ## Specified Response: any text or thoughts you would like to generate, but do NOT generate another action
-
 
         You must respond in the following format:
         ```
@@ -44,31 +41,27 @@ class TrainerAgent():
         Response: hyperparams = {"num_filters": [16], "kernel_size": 3, "hidden_sizes": [64], "learning_rate": 0.001, "epochs": 2, "batch_size": 100, "lin_dropout": 0.5, "conv_dropout": 0.25}
         ```
         ```
-        Action: Rerun
-        Response: RUN
-        ```
-        ```
         Action: Think
         Response: I am analyzing the above history and have identified that the model performs better when the hyper parameters are ...
         ```
+        You should focus on hyperparameters other than epoch to start out. Try to find the best set of Convulutional Layers, Hidden Layers, etc. before moving on to longer training runs. Please analyze the above history and metrics and respond in the specified format.
 
+        You may only think ONCE, do not generate Think action twice.
         """)
         formatting = '\n'.join([line.strip() for line in formatting.split('\n')])
 
         self.add_message(formatting, role="user")
 
-        self.add_message("Please analyze the above history and metrics and respond in the specified format.", role="user")
-        
+        if thought_message != None:
+            self.add_message(thought_message, role="assistant")
+
         """
         print("-----------------------")
         for message in self.messages:
             print(message['content'])
         print("-----------------------")
         """
-
-        if thought_message != None:
-            self.add_message(thought_message, role="assistant")
-
+        
         response = openai.ChatCompletion.create(
             model=model,
             messages=self.messages,
